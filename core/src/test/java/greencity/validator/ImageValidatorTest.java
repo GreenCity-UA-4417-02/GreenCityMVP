be9033cdb79e1f070185f565b1197979440756f3
Yaroslav Guz <aroslavguz@gmail.com>
2026-01-09 17:48:20 +0200

    Create unit test for image validator when image is null

---
+++ core/src/test/java/greencity/validator/ImageValidatorTest.java
@@ -0,0 +1,32 @@
+package greencity.validator;
+
+import jakarta.validation.ConstraintValidatorContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ImageValidatorTest {
+    @Mock
+    private MultipartFile multipartFile;
+
+    @Mock
+    private ConstraintValidatorContext context;
+
+    @InjectMocks
+    private ImageValidator imageValidator;
+
+    @Test
+    void isValid_NullImage_ReturnsTrue() {
+        boolean result = imageValidator.isValid(null, context);
+        assertTrue(result);
+    }
+}
