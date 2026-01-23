173dbf9f763524aa569b71140525d1b034b4bd07
Yaroslav Guz <aroslavguz@gmail.com>
2026-01-23 19:31:36 +0200

    feature(create-event): create custom annotation and validator for checking is date future or present

---
+++ core/src/main/java/greencity/validator/FutureOrPresentDateValidator.java
@@ -0,0 +1,13 @@
+package greencity.validator;
+
+import greencity.annotations.FutureOrPresentDate;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class FutureOrPresentDateValidator implements ConstraintValidator<FutureOrPresentDate, LocalDate> {
+    @Override
+    public boolean isValid(LocalDate date, ConstraintValidatorContext context) {
+        return date == null || !date.isBefore(LocalDate.now());
+    }
+}
