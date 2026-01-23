9e7903687c032873721c5a952bc3166a55a8f48f
Yaroslav Guz <aroslavguz@gmail.com>
2026-01-23 20:25:05 +0200

    feature(create-event): create repositories needed for event service

---
+++ dao/src/main/java/greencity/repository/EventCategoryRepository.java
@@ -0,0 +1,9 @@
+package greencity.repository;
+
+import greencity.entity.event.EventCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventCategoryRepository extends JpaRepository<EventCategory, Long> {
+}
