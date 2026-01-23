b013d3753b68d1544a8461a751f07e05a9a2d62d
Yaroslav Guz <aroslavguz@gmail.com>
2026-01-23 19:36:01 +0200

    feature(create-event): create custom annotation and validator for future checking event type

---
+++ core/src/main/java/greencity/annotations/ValidEventTypeConsistency.java
@@ -0,0 +1,17 @@
+package greencity.annotations;
+
+import greencity.validator.ValidEventTypeConsistencyValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ValidEventTypeConsistencyValidator.class)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEventTypeConsistency {
+    String message() default "Event type and location/link consistency error";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
+
