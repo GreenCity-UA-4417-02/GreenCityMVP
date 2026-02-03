fdb85d4bde33731497b1421211cd7b0a3852c509
AlonaZahychenko <zahychenko.alona8@gmail.com>
2026-02-03 13:54:25 +0200

    feat(notification): allow USER access to notifications endpoint
    DEV-14

--- core/src/main/java/greencity/controller/NotificationController.java
+++ core/src/main/java/greencity/controller/NotificationController.java
@@ -1,14 +1,15 @@
 package greencity.controller;
 
+import greencity.annotations.CurrentUser;
 import greencity.dto.PageableDto;
 import greencity.dto.notification.NotificationResponseDto;
+import greencity.dto.user.UserVO;
 import greencity.service.NotificationService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +21,7 @@ public class NotificationController {
 
     @GetMapping
     public PageableDto<NotificationResponseDto> getAllNotificationsForUser(
-            @RequestParam Long userId, @PageableDefault(value = 20) Pageable pageable) {
-        return notificationService.getAllNotificationsForUser(userId, pageable);
+            @CurrentUser UserVO userVO, @PageableDefault(value = 20) Pageable pageable) {
+        return notificationService.getAllNotificationsForUser(userVO.getId(), pageable);
     }
 }
