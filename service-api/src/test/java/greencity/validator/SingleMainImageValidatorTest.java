7f78376da534a1c47acd7687a8987dbf9ac93805
Yaroslav Guz <aroslavguz@gmail.com>
2026-02-08 13:31:10 +0200

    test(create-event): create tests for SingleMainImageValidator

---
+++ service-api/src/test/java/greencity/validator/SingleMainImageValidatorTest.java
@@ -0,0 +1,35 @@
+package greencity.validator;
+
+import greencity.ModelUtils;
+import greencity.dto.event.CreateEventRequestDto;
+import greencity.dto.event.EventImageDto;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleMainImageValidatorTest {
+    private final SingleMainImageValidator validator = new SingleMainImageValidator();
+
+    @Test
+    void isValid_MultipleMainImages_ReturnsFalse() {
+        List<EventImageDto> images = List.of(
+                new EventImageDto("http://img1.jpg", true),
+                new EventImageDto("http://img2.jpg", true)
+        );
+        CreateEventRequestDto request = new CreateEventRequestDto("T", List.of(), "D", null, images, 1L, 1L, true);
+
+        assertFalse(validator.isValid(request, null));
+    }
+
+    @Test
+    void isValid_SingleMainImage_ReturnsTrue() {
+        CreateEventRequestDto request = ModelUtils.getCreateEventRequestDto();
+        assertTrue(validator.isValid(request, null));
+    }
+
+    @Test
+    void isValid_NoImages_ReturnsTrue() {
+        CreateEventRequestDto request = new CreateEventRequestDto("T", List.of(), "D", null, List.of(), 1L, 1L, true);
+        assertTrue(validator.isValid(request, null));
+    }
+}
