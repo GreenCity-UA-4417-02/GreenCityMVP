6115aabd5a9886096ffd238669b41504723509e5
Yaroslav Guz <aroslavguz@gmail.com>
2026-02-08 15:02:43 +0200

    fix(create-event): orverride equals, hashCode and toString in EventImageContentDto

--- service-api/src/main/java/greencity/dto/event/EventImageContentDto.java
+++ service-api/src/main/java/greencity/dto/event/EventImageContentDto.java
@@ -2,6 +2,8 @@ package greencity.dto.event;
 
 import lombok.Builder;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 
 @Builder
@@ -10,4 +12,32 @@ public record EventImageContentDto(
         byte[] imageData,
         String contentType,
         String link
-) {}
+) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventImageContentDto debt = (EventImageContentDto) o;
+        return Objects.equals(id, debt.id) &&
+               Arrays.equals(imageData, debt.imageData) &&
+               Objects.equals(contentType, debt.contentType) &&
+               Objects.equals(link, debt.link);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, contentType, link);
+        result = 31 * result + Arrays.hashCode(imageData);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "EventImageContentDto{" +
+               "id=" + id +
+               ", imageData=" + Arrays.toString(imageData) +
+               ", contentType='" + contentType + '\'' +
+               ", link='" + link + '\'' +
+               '}';
+    }
+}
