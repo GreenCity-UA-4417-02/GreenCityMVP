77645e26b70901bf1699284a98cf713c041f1940
Sviatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2026-02-19 17:32:06 -0500

    feat(notification): add soft delete for notifications

--- dao/src/main/java/greencity/repository/NotificationRepo.java
+++ dao/src/main/java/greencity/repository/NotificationRepo.java
@@ -6,10 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepo extends JpaRepository<Notification, Long> {
 
     Page<Notification> findAllByRecipientUserIdOrderByCreatedAtDesc(Long recipientUserId, Pageable page);
 
     long countByRecipientUserIdAndReadAtIsNull(Long recipientUserId);
+
+    List<Notification> findAllByrecipientUserIdAndIsDeletedFalse(Long recipientUserId);
 }
--- service/src/main/java/greencity/service/NotificationServiceImpl.java
+++ service/src/main/java/greencity/service/NotificationServiceImpl.java
@@ -4,6 +4,7 @@ import greencity.client.RestClient;
 import greencity.dto.PageableDto;
 import greencity.dto.notification.NotificationResponseDto;
 import greencity.entity.Notification;
+import greencity.exception.exceptions.NotFoundException;
 import greencity.mapping.NotificationDtoResponseMapper;
 import greencity.repository.NotificationRepo;
 import lombok.RequiredArgsConstructor;
@@ -27,36 +28,33 @@ public class NotificationServiceImpl implements NotificationService {
 
     public PageableDto<NotificationResponseDto> getAllNotificationsForUser(Long userId, Pageable page) {
         Page<Notification> notifications = notificationRepo
-                .findAllByRecipientUserIdOrderByCreatedAtDesc(userId, page);
+            .findAllByRecipientUserIdOrderByCreatedAtDesc(userId, page);
 
         if (notifications.isEmpty()) {
             return new PageableDto<>(Collections.emptyList(), 0, 0, 0);
         }
 
         Set<Long> actorIds = notifications.getContent()
-                .stream()
-                .map(Notification::getActorUserId)
-                .collect(Collectors.toSet());
+            .stream()
+            .map(Notification::getActorUserId)
+            .collect(Collectors.toSet());
 
         final Map<Long, String> actorNames = getActorNamesSafety(actorIds);
 
         List<NotificationResponseDto> responseDtoList = notifications
-                .getContent()
-                .stream()
-                .map(notification ->
-                        notificationDtoResponseMapper
-                                .convert(notification,
-                                        actorNames.getOrDefault(
-                                                notification.getActorUserId(), "Unknown user")
-                                )
-                )
-                .toList();
+            .getContent()
+            .stream()
+            .map(notification -> notificationDtoResponseMapper
+                .convert(notification,
+                    actorNames.getOrDefault(
+                        notification.getActorUserId(), "Unknown user")))
+            .toList();
 
         return new PageableDto<>(
-                responseDtoList,
-                notifications.getTotalElements(),
-                notifications.getNumber(),
-                notifications.getTotalPages());
+            responseDtoList,
+            notifications.getTotalElements(),
+            notifications.getNumber(),
+            notifications.getTotalPages());
     }
 
     private Map<Long, String> getActorNamesSafety(Set<Long> actorIds) {
@@ -66,5 +64,13 @@ public class NotificationServiceImpl implements NotificationService {
         } catch (Exception e) {
             return Collections.emptyMap();
         }
+    }
+
+    public void delete(Long id) {
+        Notification notification = notificationRepo.findById(id)
+            .orElseThrow(() -> new NotFoundException("Notification not found"));
+
+        notification.setDeleted(true);
+        notificationRepo.save(notification);
     }
 }
